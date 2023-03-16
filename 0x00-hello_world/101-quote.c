31b8b2d7fcbb636ecf161662e3e2cbc902dcf45e
ayoub <ayoubaitelaouad@gmail.com>
2023-03-16 22:59:29 +0100

    hot fix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-        char data[] = "and that piece of art is useful\" - Dora Korpar,2015-10-19\n";
+        char data[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
         fwrite(data, sizeof(char), 59, stderr);
         return(1);
