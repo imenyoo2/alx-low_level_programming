20032eb619c3492d815c318eee3e79f0212a8a10
ayoub <ayoubaitelaouad@gmail.com>
2023-03-16 12:44:02 +0100

    hot fix

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,16 +1,20 @@
 #include <stdio.h>
 
-int main()
+/**
+ * this is a function that prints the size of basic types in c
+ */
+int main(void)
 {
-  char a;
-  int b;
-  long int c;
-  long long int d;
-  float e;
-  printf("Size of a char: %d byte(s)\n", sizeof(a));
-  printf("Size of an int: %d byte(s)\n", sizeof(b));
-  printf("Size of a long int: %d byte(s)\n", sizeof(c));
-  printf("Size of a long long int: %d byte(s)\n", sizeof(d));
-  printf("Size of a float: %d byte(s)\n", sizeof(e));
-  return 0;
+char a;
+int b;
+long int c;
+long long int d;
+float e;
+
+printf("Size of a char: %d byte(s)\n", sizeof(a));
+printf("Size of an int: %d byte(s)\n", sizeof(b));
+printf("Size of a long int: %d byte(s)\n", sizeof(c));
+printf("Size of a long long int: %d byte(s)\n", sizeof(d));
+printf("Size of a float: %d byte(s)\n", sizeof(e));
+return 0;
 }
