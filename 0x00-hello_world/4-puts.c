44e1d0679b3223c1533e1d70399f967a3f636bd4
ayoub <ayoubaitelaouad@gmail.com>
2023-03-16 22:19:07 +0100

    hot fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 
+/**
+ * main - prints a string
+ * @parameterx: none
+ * Return:  coind
+ */
 int main(void)
 {
 char out[52] = "\"Programming is like building a multilingual puzzle";
 
 puts(out);
-return 0;
+{return 0; }
 }
