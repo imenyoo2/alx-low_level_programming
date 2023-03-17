ba895f3e6ed40491a240f880c796889f7a419a05
ayoub <ayoubaitelaouad@gmail.com>
2023-03-17 16:29:58 +0100

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: 0
+ */
+int main(void)
+{
+char a = '0';
+
+while (a <= '9')
+{
+putchar(a);
+a += 1;
+}
+putchar('\n');
+
+{return 0; }
+}
