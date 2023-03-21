e0e56c004cc64894d965bc60ed650fc9515b39a7
ayoub <ayoubaitelaouad@gmail.com>
2023-03-21 16:06:31 +0000

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * print_alphabet_x10 - prints alphabets 10 times
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("abcdefghijklmnopqrstuvwxyz\n");
+	}
+}
