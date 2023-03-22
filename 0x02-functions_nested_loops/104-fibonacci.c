a46ff707cc716876fa959411573fce4477bd62a7
ayoub <ayoubaitelaouad@gmail.com>
2023-03-22 00:39:25 +0000

    104-fibonacci.c

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+
+/**
+ * main - Entry point
+ * Return: always 0;
+ */
+int main(void)
+{
+	unsigned long int n1;
+	unsigned long int n2;
+	unsigned long int buffer;
+	int i;
+
+	printf("1, 2");
+	n1 = 1;
+	n2 = 2;
+	for (i = 0; i < 96; i++)
+	{
+		buffer = n1 + n2;
+		printf(", %lu", buffer);
+		n1 = n2;
+		n2 = buffer;
+	}
+	printf("\n");
+	return (0);
+}
