ce110311ff2861225bfdf2af49ce11ff31e22607
ayoub <ayoubaitelaouad@gmail.com>
2023-03-23 22:28:28 +0000

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints number from 0 to 9 followed by new line
+ * Return: Always 0
+ */
+void print_most_numbers(void)
+{
+	char i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		if (i != '2' && i != '4')
+		{
+			_putchar(i);
+		}
+	}
+	_putchar('\n');
+}
