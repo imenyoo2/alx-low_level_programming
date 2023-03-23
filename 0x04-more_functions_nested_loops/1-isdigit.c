6011cf8512291d7d8c59e0040129aaa0adb3f95a
ayoub <ayoubaitelaouad@gmail.com>
2023-03-23 21:34:57 +0000

    1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+
+/**
+ * _isdigit - checks if c is a digite
+ * @c: the ASCII code of the char to be checked
+ * Return: 1 if true, otherwise 0
+ */
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
