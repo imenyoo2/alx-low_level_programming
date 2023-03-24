ce3fc3387aae27d09bbaae6e78daffc70001ca61
ayoub <ayoubaitelaouad@gmail.com>
2023-03-24 01:20:10 +0000

    hot fox

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -13,13 +13,13 @@ void more_numbers(void)
 	for (j = 0; j < 10; j++)
 	{
 		c = '0';
-		for (i = 0; i < 14; i++)
+		for (i = 0; i < 15; i++)
 		{
-			if (i == 9)
+			if (i == 10)
 			{
 				c = '0';
 			}
-			if (i >= 9)
+			if (i >= 10)
 			{
 				_putchar('1');
 			}
--- 0x04-more_functions_nested_loops/main.c
+++ 0x04-more_functions_nested_loops/main.c
@@ -7,15 +7,6 @@
  */
 int main(void)
 {
-	print_number(98);
-	_putchar('\n');
-	print_number(402);
-	_putchar('\n');
-	print_number(1024);
-	_putchar('\n');
-	print_number(0);
-	_putchar('\n');
-	print_number(-98);
-	_putchar('\n');
+	more_numbers();
 	return (0);
 }
