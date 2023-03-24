e704f557c5d1a0fcf417fddb5c7d39931f720ec8
ayoub <ayoubaitelaouad@gmail.com>
2023-03-24 01:06:40 +0000

    hot fix

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,20 +7,16 @@
  */
 void print_number(int n)
 {
-	int lastDigit;
+	unsigned int num = n;
 
 	if (n < 0)
 	{
-		n *= -1;
+		_putchar('-');
+		num = -num;
 	}
-	if (n < 10)
-	{
-		_putchar('0' + n);
-	}
-	else
-	{
-		lastDigit = n % 10;
-		print_number(n / 10);
-		_putchar('0' + lastDigit);
-	}
+
+	if ((num / 10) > 0)
+		print_number(num / 10);
+
+	_putchar((num % 10) + 48);
 }
