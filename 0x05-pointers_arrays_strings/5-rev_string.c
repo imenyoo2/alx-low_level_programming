757e3616ffd34f5af978c221c1165878bb8c2bca
ayoub <ayoubaitelaouad@gmail.com>
2023-03-28 23:51:21 +0000

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+void rev_string(char *s)
+{
+	char *start = s;
+	int length = _strlen(s);
+	char *end = s + length - 1;
+	int count = 0;
+	char buffer;
+
+	while (count < (length) / 2)
+	{
+		buffer = *(start + count);
+		*(start + count) = *(end - count);
+		*(end - count) = buffer;
+		count++;
+	}
+
+}
+
+/**
+ * _strlen - return the length of the string s
+ * @s: the pointer to the first head of the string
+ * Return: the length
+ */
+int _strlen(char *s)
+{
+	int length = 0;
+	char *count = s;
+
+	while (*count != '\0')
+	{
+		length++;
+		count++;
+	}
+	return (length);
+}
