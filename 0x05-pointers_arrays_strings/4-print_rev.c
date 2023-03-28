69318e17c6a552159126e9175b99b2a95dc20c72
ayoub <ayoubaitelaouad@gmail.com>
2023-03-28 23:34:52 +0000

    hot fix

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -16,3 +16,21 @@ void print_rev(char *s)
 	}
 	_putchar('\n');
 }
+
+/**
+ * _strlen - return the length of the string s
+ * @s: the pointer to the first head of the string
+ * Return: the length
+ */
+int _strlen(char *s)
+{
+	int length = 0;
+	char *count = s;
+
+	while (*count != '\0')
+	{
+		length++;
+		count++;
+	}
+	return (length);
+}
