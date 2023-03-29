230e76cb4e1b50e8f4e59f41cb8c443feb453783
ayoub <ayoubaitelaouad@gmail.com>
2023-03-29 00:15:19 +0000

    7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string
+ * @str: string
+ * Return: void
+ */
+void puts_half(char *str)
+{
+	char length = _strlen(str);
+	int n;
+
+	if (length % 2)
+	{
+		n = (length - 1) / 2;
+	}
+	else
+	{
+		n = length / 2;
+	}
+
+	while (n < length)
+	{
+		_putchar(*(str + n));
+		n++;
+	}
+	_putchar('\n');
+}
+
+/**
+ * _strlen - return the length of the string s
+ * @s: the pointer to the first head of the string
+ * Return: the length
+ */
+int _strlen(char *s)
+{
+	int length = 0;
+	char *count = s;
+
+	while (*count != '\0')
+	{
+		length++;
+		count++;
+	}
+	return (length);
+}
