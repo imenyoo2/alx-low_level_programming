bf6123db3ea40ffc72517a96d950eb91a3404231
ayoub <ayoubaitelaouad@gmail.com>
2023-04-03 13:58:38 +0000

    2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * _strchr - locates a character in a string
+ * @s: the string
+ * @c: the character
+ * Return: pointer to the first occurrence of c, or NULL if 
+ * no character is found
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
+	}
+	return (NULL);
+}
