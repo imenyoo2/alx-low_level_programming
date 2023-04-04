3af7f939ebc581828cba580bb925b78b94a5a7d2
ayoub <ayoubaitelaouad@gmail.com>
2023-04-04 18:45:21 +0000

    7-print_chessboard.c

---
+++ 0x07-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _putchar - clone of putchar
+ * @c: the ASCII code of the character
+ * Return: void
+ */
+void _putchar(char c)
+{
+	putchar(c);
+}
