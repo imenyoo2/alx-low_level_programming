a7826c3df71f16e2a3cb5ab9e7c302fb5d664e23
ayoub <ayoubaitelaouad@gmail.com>
2023-04-05 23:54:32 +0000

    3-factorial.c

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,18 @@
+
+/**
+ * factorial - calculates the factorial of n
+ * @n: integer
+ * Return: factorial of n
+ */
+int factorial(int n)
+{
+	if (n == 1 || n == 0)
+	{
+		return (1);
+	}
+	else if (n < 0)
+	{
+		return (-1);
+	}
+	return (n * factorial(n - 1));
+}
