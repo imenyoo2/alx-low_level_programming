ffe7910e09dca32bffe0ab0c0a60c07d5ba20d6f
ayoub <ayoubaitelaouad@gmail.com>
2023-04-06 01:01:20 +0000

    6-is_prime_number.c

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,37 @@
+
+int is_prime(int n, int i);
+/**
+ * is_prime_number - checks if n is a prime number
+ * @n: the number to be checked
+ * Return: 1 if true, otherwise 0
+ */
+int is_prime_number(int n)
+{
+	if (n < 2)
+	{
+		return (0);
+	}
+	else
+	{
+		return (is_prime(n, n - 1));
+	}
+}
+
+int is_prime(int n, int i)
+{
+	if (i != 1)
+	{
+		if (n % i == 0)
+		{
+			return (0);
+		}
+		else
+		{
+			return (is_prime(n, i - 1));
+		}
+	}
+	else
+	{
+		return (1);
+	}
+}
