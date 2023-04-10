ef83a3e4e8b6c6785737a3659fea8e6aeb34fd41
ayoub <ayoubaitelaouad@gmail.com>
2023-04-10 23:53:39 +0000

    betty doc

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,6 +2,12 @@
 #include "main.h"
 #include <stdio.h>
 
+/**
+ * main - entry point
+ * @argc: size of argv
+ * @argv: array of arguments passed
+ * Return: always 0
+ */
 int main(int argc, char **argv)
 {
 	(void)argc;
