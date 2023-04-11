191704ac8f765f1a05f551dff3d751e740ceffd0
ayoub <ayoubaitelaouad@gmail.com>
2023-04-11 00:11:01 +0000

    3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ * @argc: the number of args passed
+ * @argv: array of args passed
+ * Return: Always 0
+ */
+int main(int argc, char **argv)
+{
+	if (argc == 3)
+	{
+		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+		return (0);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+}
