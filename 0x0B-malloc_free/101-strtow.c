57fa84deff1caaf622f8f2abc0c1b73103aca9af
ayoub <ayoubaitelaouad@gmail.com>
2023-04-12 16:32:41 +0000

    101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,6 +1,8 @@
 #include <stdlib.h>
 #include "main.h"
 
+char *_strdump(char *start, char *end);
+
 /**
  * strtow - convert str to an array of words
  * @str: string
@@ -9,25 +11,45 @@
 char **strtow(char *str)
 {
 	char **arr;
-	int i;
+	int i, state = 1;
 	char *start;
+	char *end;
 
 	if (str == NULL || *str == '\n')
 	{
 		return (NULL);
 	}
-	arr = (char **) malloc(numOfWords(str) * sizeof(char *));
-
-	for (i = 0; i < numOfWords(str); i++)
+	arr = (char **) malloc((numOfWords(str) + 1) * sizeof(char *));
+	if (arr == NULL)
 	{
-		while (1)
-		{
-			if ()
-		}
+		return (NULL);
 	}
 
-
+	i = 0;
+	while (*str != '\0')
+	{
+		if ((*str != ' ' && *str != '\n' && *str != '\t') && state)
+		{
+			start = str;
+			state = 0;
+		}
+		else if ((*str == ' ' || *str == '\n' || *str == '\t') && (!state))
+		{
+			end = str;
+			arr[i] = _strdump(start, end);
+			if (arr[i] == NULL)
+			{
+				return (NULL);
+			}
+			state = 1;
+			i++;
+		}
+		str++;
+	}
+	arr[i] = NULL;
+	return (arr);
 }
+
 
 /**
  * numOfWords - evaluates the number of words in str
@@ -40,15 +62,45 @@ int numOfWords(char *str)
 	int state = 0;
 	int i;
 
-	for (i = 0; str[i] != '\0'; i++) {
-		if (str[i] == ' ' || str[i] == '\n' || str[i] == '\t') {
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] == ' ' || str[i] == '\n' || str[i] == '\t')
+		{
 			state = 0;
 		}
-		else if (state == 0) {
+		else if (state == 0)
+		{
 			state = 1;
 			count++;
 		}
 	}
 
 	return (count);
+}
+
+/**
+ * _strdump - return a new chunk of memory containing str
+ * @start: starting of string
+ * @end: ending of string
+ * Return: pointer to the new string
+ */
+char *_strdump(char *start, char *end)
+{
+	int size = end - start;
+	char *new;
+	int i;
+
+	/* alocating memory */
+	new = (char *) malloc(size + 1);
+	if (new == NULL)
+	{
+		return (NULL);
+	}
+	/* dumping str to new */
+	for (i = 0; i < size; i++)
+	{
+		new[i] = start[i];
+	}
+	new[i] = '\0';
+	return (new);
 }
