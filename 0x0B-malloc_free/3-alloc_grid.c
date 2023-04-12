285a170fee966fd5665f8ad00dba399f80370699
ayoub <ayoubaitelaouad@gmail.com>
2023-04-12 00:47:59 +0000

    hot fix

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -24,18 +24,15 @@ int **alloc_grid(int width, int height)
 	}
 	for (i = 0; i < height; i++)
 	{
-		arr[i] = (int *) malloc(width * sizeof(int));
+		arr[i] = (int *) calloc(width, sizeof(int));
 		if (arr[i] == NULL)
 		{
-			for (j = 0; j <= i; j++)
+			for (j = 0; j < i; j++)
 			{
 				free(arr[j]);
 			}
+			free(arr);
 			return (NULL);
-		}
-		for (j = 0; j < width; j++)
-		{
-			arr[i][j] = 0;
 		}
 	}
 	return (arr);
