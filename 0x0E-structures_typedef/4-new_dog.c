f990774f51b3bb6686b94c93c104f186a7633da2
ayoub <ayoubaitelaouad@gmail.com>
2023-04-18 00:54:09 +0000

    4-new_dog.c

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,20 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * new_dog - creates a new dog instence
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ * Return: dog_t
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *new;
+
+	new = malloc(sizeof(new));
+	new->name = name;
+	new->age = age;
+	new->owner = owner;
+	return (new);
+}
