3fe2287d083bf688b4dbe15867b99d57e2df411a
ayoub <ayoubaitelaouad@gmail.com>
2023-04-18 00:21:22 +0000

    document dog struct

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,14 @@
+#include "dog.h"
+
+/**
+ * init_dog - initializes the struct d with passed arguments
+ * @d: dog instance
+ * @name: name field
+ * @age: name age
+ * @owner: name owner
+ * Return: void
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+
+}
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,12 @@
 #ifndef DOG
 #define DOG
 
+/**
+ * struct dog - represent a dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ */
 struct dog
 {
 	char *name;
