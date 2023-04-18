19a390817a099a0b07fac7750982e8db126d7f20
ayoub <ayoubaitelaouad@gmail.com>
2023-04-18 00:56:53 +0000

    try fix

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -13,6 +13,10 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog_t *new;
 
 	new = malloc(sizeof(new));
+	if (new == NULL)
+	{
+		return (NULL);
+	}
 	new->name = name;
 	new->age = age;
 	new->owner = owner;
