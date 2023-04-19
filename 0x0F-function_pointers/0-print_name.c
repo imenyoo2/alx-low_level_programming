63a959f28032897e33ce9de6b47092e4f100d6cb
ayoub <ayoubaitelaouad@gmail.com>
2023-04-19 23:38:35 +0000

    try fix

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,4 @@
-
+#include <stdlib.h>
 
 /**
  * print_name - prints a name
@@ -8,5 +8,9 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
+	if (name == NULL || f == NULL)
+	{
+		return;
+	}
 	(*f)(name);
 }
