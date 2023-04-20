6dce98f7d4e16f44629f6866b73123e06c2f958e
ayoub <ayoubaitelaouad@gmail.com>
2023-04-20 23:55:54 +0000

    try fix

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,5 +1,6 @@
 #include <stdarg.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * print_strings - prints strings
@@ -11,7 +12,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 {
 	va_list ap;
 	unsigned int i;
-	char *buffer;
+	char **array = malloc(n * sizeof(char *));
 
 	if (separator == NULL)
 	{
@@ -20,12 +21,21 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	if (n >= 1)
 	{
 		va_start(ap, n);
-		printf("%s", va_arg(ap, char *));
+		for (i = 0; i < n; i++)
+		{
+			array[i] = va_arg(ap, char *);
+			if (array[i] == NULL)
+			{
+				printf("(nil)\n");
+				return;
+			}
+		}
+		printf("%s", array[0]);
 		for (i = 1; i < n; i++)
 		{
-			buffer = va_arg(ap, char *);
-			printf("%s%s", separator, (buffer == NULL?"(nil)":buffer));
+			printf("%s%s", separator, array[i]);
 		}
+		free(array);
 		va_end(ap);
 	}
 	printf("\n");
