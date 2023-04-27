44493abd8931d66a232978f33de9c88102613159
ayoub <ayoubaitelaouad@gmail.com>
2023-04-27 13:09:43 +0100

    fix

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -12,19 +12,18 @@ size_t print_list(const list_t *h)
 
 	if (h == NULL)
 	{
-		return (-1);
+		return (0);
 	}
 	while (1)
 	{
 		elements++;
-		printf("[%d] ", h->len);
 		if (h->str == NULL)
 		{
-			printf("(nil)\n");
+			printf("[0] (nil)\n");
 		}
 		else
 		{
-			printf("%s\n", h->str);
+			printf("[%d] %s\n", h->len, h->str);
 		}
 
 		if (h->next == NULL)
