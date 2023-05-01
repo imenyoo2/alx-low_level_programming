b851c2ecbfe9eae5af3170a5ee46d63980312ca6
ayoub <ayoubaitelaouad@gmail.com>
2023-05-01 23:49:49 +0100

    9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -41,7 +41,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		}
 		idx--;
 	}
-	new->next = buffer->next->next;
+	new->next = buffer->next;
 	buffer->next = new;
 	return (new);
 }
