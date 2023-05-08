e3531e4dfb52e5698577ca2b6e1cf9e6aa106b96
ayoub <ayoubaitelaouad@gmail.com>
2023-05-08 22:59:16 +0100

    try fix

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -37,8 +37,5 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	}
 	free(buffer);
 	close(fd);
-	if (result != result2)
-		return (result2);
-	else
-		return (result);
+	return (result2);
 }
