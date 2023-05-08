54e7d1ff0563a497ef9d7cbb2d662d59ff2b7306
ayoub <ayoubaitelaouad@gmail.com>
2023-05-08 23:37:13 +0100

    try fix

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,7 +10,7 @@
 int create_file(const char *filename, char *text_content)
 {
 	int fd;
-	
+
 	if (filename == NULL)
 	{
 		return (-1);
@@ -21,7 +21,11 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	}
 
-	if (write(fd, text_content, sizeof(text_content)) == -1)
+	if (text_content == NULL)
+	{
+		return (1);
+	}
+	else if (write(fd, text_content, sizeof(text_content)) == -1)
 	{
 		return (-1);
 	}
