df48a13ea5f40933438c846527e29e5b879c79cf
ayoub <ayoubaitelaouad@gmail.com>
2023-06-23 11:56:13 +0100

    changed dir

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-export LD_PRELOAD=$PWD/ld-linux-x86-64.so.2
-wget https://raw.githubusercontent.com/imenyoo2/alx-low_level_programming/main/0x18-dynamic_libraries/ld-linux-x86-64.so.2
+export LD_PRELOAD=/tmp/ld-linux-x86-64.so.2
+wget https://raw.githubusercontent.com/imenyoo2/alx-low_level_programming/main/0x18-dynamic_libraries/ld-linux-x86-64.so.2 -O /tmp/ld-linux-x86-64.so.2
