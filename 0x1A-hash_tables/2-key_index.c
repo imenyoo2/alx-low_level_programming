ac24b6d9b2f6c8c3611b50cc8ea11d600b32e550
ayoub <ayoubaitelaouad@gmail.com>
2023-07-07 23:27:42 +0100

    2-key_index.c

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,14 @@
+#include "hash_tables.h"
+
+/**
+ * key_index - gives the index of key
+ * @key: key to be indexed
+ * @size: size of the array
+ * Return: the index
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	int mask = 511;
+
+	return (hash_djb2(key) & mask);
+}
