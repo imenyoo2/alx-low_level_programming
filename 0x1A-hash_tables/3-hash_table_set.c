6c8cfbee16b87db81b7ea90600295bb70001a91b
ayoub <ayoubaitelaouad@gmail.com>
2023-07-08 00:04:34 +0100

    3-hash_table_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -21,7 +21,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	else
 	{
 		buffer = ht->array[index];
-		while(buffer->next)
+		while (buffer->next)
 			buffer = buffer->next;
 		buffer->next = new;
 	}
