45cb9953203c7b9273631d8b8f93af53f4c0434b
ayoub <ayoubaitelaouad@gmail.com>
2023-07-08 19:37:36 +0100

    4-hash_table_get.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,5 +1,4 @@
 #include "hash_tables.h"
-#include <stdio.h>
 
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
@@ -13,13 +12,14 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (0);
 
 	index = key_index((unsigned const char *) key, ht->size);
-	printf("%u\n", index);
 	new->key = strdup(key);
 	new->value = strdup(value);
 	new->next = NULL;
 
 	if ((ht->array)[index] == NULL)
+	{
 		(ht->array)[index] = new;
+	}
 	else
 	{
 		buffer = (ht->array)[index];
---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,19 @@
+#include "hash_tables.h"
+
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	int index;
+
+	if (key == NULL || *key == '\0')
+	{
+		return (NULL);
+	}
+	index = key_index((const unsigned char *)key, ht->size);
+	if (index >= ht->size)
+		return (NULL);
+
+	if ((ht->array)[index] == NULL)
+		return (NULL);
+
+	return ((ht->array)[index]->value);
+}
