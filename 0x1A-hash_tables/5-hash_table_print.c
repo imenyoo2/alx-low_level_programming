ebcbb11387b251addfd4a35c706a7d047939f2d7
ayoub <ayoubaitelaouad@gmail.com>
2023-07-08 20:37:49 +0100

    betty doc

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -1,5 +1,10 @@
 #include "hash_tables.h"
 
+/**
+ * hash_table_print - prints the hash table
+ * @ht: the hash table
+ * Return: void
+ */
 void hash_table_print(const hash_table_t *ht)
 {
 	hash_node_t *buffer;
