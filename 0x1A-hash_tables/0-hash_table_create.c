854742c90d936de9dee349ecaef76df628210cbd
ayoub <ayoubaitelaouad@gmail.com>
2023-09-07 19:52:46 +0100

    fix

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,12 +9,14 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *table;
+	hash_table_t **array;
 
 	table = malloc(sizeof(hash_table_t));
-	if (table == NULL)
+	array = malloc(sizeof(hash_node_t *) * size);
+	if (table == NULL || array == NULL)
 		return (NULL);
 
 	table->size = size;
-	table->array = NULL;
+	table->array = array;
 	return (table);
 }
