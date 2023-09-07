173e006d279a50232319d6628f2cebe209fe54fa
ayoub <ayoubaitelaouad@gmail.com>
2023-09-07 19:41:13 +0100

    first commit

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,19 @@
+
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - creats a new table
+ * @size: size of table
+ * Return: pointer of NULL
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *table;
+
+	table = malloc(sizeof(hash_table_t));
+	if (table == NULL)
+		return (NULL);
+	
+	table->size = size;
+	return (table);
+}
