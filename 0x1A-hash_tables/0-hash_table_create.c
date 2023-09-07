483d1a1a3f9622d9ae795f0fdc8b5a84b558eab6
ayoub <ayoubaitelaouad@gmail.com>
2023-09-07 19:48:43 +0100

    0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -13,7 +13,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	table = malloc(sizeof(hash_table_t));
 	if (table == NULL)
 		return (NULL);
-	
+
 	table->size = size;
 	return (table);
 }
